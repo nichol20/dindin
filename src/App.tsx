fd51b92de891736c40e49e0a9db3139b5209d2ac
Vinicius Montenegro <vini_1899@hotmail.com>
2024-07-30 19:14:01 -0300

    add RegisterPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/Login";
+import RegisterPage from "./pages/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Register.tsx
@@ -0,0 +1,26 @@
+import { Header } from '../components/Header'
+import { InputField } from '../components/InputField'
+
+import styles from '../styles/Register.module.scss'
+
+export default function RegisterPage() {
+    return (
+        <div className={styles.registerPage}>
+            <Header />
+            <main>
+                <section className={styles.registerSection}>   
+                    <h1>Cadastre-se</h1>
+                    <form className={styles.registerForm}>
+                        <InputField inputId='nome' name='nome' type='text'/>
+                        <InputField inputId='email' name='email' type='email' />
+                        <InputField inputId='password' name='senha' type='password' />
+                        <InputField inputId='password' name='confirmação de senha' type='password' />
+                        <button className={styles.submitBtn}>Cadastrar</button>
+                    </form>
+
+                    <a className={styles.loginLink} href="/login">Já tem cadastro? Clique aqui!</a>
+                </section>
+            </main>
+        </div>
+    )
+}
