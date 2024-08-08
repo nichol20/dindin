860360ab9196bc8cc7331cec71d0c991115be21e
Vinicius Montenegro <vini_1899@hotmail.com>
2024-08-08 19:05:02 -0300

    add record functions

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -24,3 +24,55 @@ export const getUser = async (token: string): Promise<User> => {
     })
     return res.data
 }
+
+interface Category {
+    id: number,
+    descricao: string
+}
+
+export const getCategories = async () => {
+    const res = await http.get<Category[]>("/categoria");
+    return res.data
+};
+
+export const deleteRecord = async (id:number) => {
+    await http.delete(`/transacao/${id}`);
+};
+
+interface CreateRecordParams {
+    tipo: 'entrada' | 'saida',
+    descricao: string,
+    valor: number,
+    data: string,
+    categoria_id: number
+}
+
+interface Record {
+    id: number,
+    tipo: 'entrada' | 'saida',
+    descricao: string,
+    valor: number,
+    data: string,
+    usuario_id: number,
+    categoria_id: number,
+    categoria_nome: string,
+}
+
+export const createRecord = async(params:CreateRecordParams) => {
+    const res = await http.post<Record>("/transacao",params);
+    return res.data;
+}
+
+
+interface EditRecordParams {
+    descricao: string,
+    valor: number,
+    data: string,
+    categoria_id: number,
+    tipo: 'entrada' | 'saida'
+};
+
+
+export const editRecord = async (id:number,params:EditRecordParams) => {
+    await http.put(`/transacao/${id}`,params);
+};
